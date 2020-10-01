56a8e9ccf15b89f8929548dcca07172b370899b1
Omkar Chandorkar <forumomkar@gmail.com>
2020-10-01 13:10:02 +0530

    Le roi est mort, vive le roi!
    (our meme way of saying aosip -> yaap)

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, vendor/oneplus/hotdogb/hotdogb-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosip
+    $(LOCAL_PATH)/overlay-yaap
 
 # A/B
 AB_OTA_PARTITIONS += \
